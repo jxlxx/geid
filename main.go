16b2cf8233758a54fe46e7cab67c9f8ed3bf9f02
jxlxx <wormperson69@gmail.com>
2024-01-09 12:17:19 -0500

    :tada: init v1

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"os"
+	"text/template"
+	"time"
+
+	flag "github.com/spf13/pflag"
+	"gopkg.in/yaml.v3"
+)
+
+type id struct {
+	Name   string `yaml:"name"`
+	Prefix string `yaml:"prefix"`
+}
+
+type conf struct {
+	Package string `yaml:"package"`
+	IDs     []id   `yaml:"ids"`
+	Epoch   *Epoch `yaml:"epoch,omitempty"`
+}
+
+type Epoch struct {
+	Year  int        `yaml:"year"`
+	Month time.Month `yaml:"month"`
+	Day   int        `yaml:"day"`
+}
+
+func main() {
+	flag.Parse()
+
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	c := conf{}
+	if err := yaml.Unmarshal(input, &c); err != nil {
+		panic(err)
+	}
+	if c.Epoch == nil {
+		c.Epoch = &Epoch{
+			Year:  1970,
+			Month: time.January,
+			Day:   0,
+		}
+	}
+
+	tmpl, err := template.New("ids.gotmpl").ParseFiles("ids.gotmpl")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.ExecuteTemplate(os.Stdout, "ids.gotmpl", c)
+	if err != nil {
+		panic(err)
+	}
+}
+
+var filename string
+
+func init() {
+	flag.StringVar(&filename, "config", "c", "input file name")
+}
